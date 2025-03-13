a0a55f964c1228452e785d0d31b770adc29372cf
wu5bocheng <wu5bocheng@gmail.com>
2025-03-12 22:48:31 -0700

    add participants

--- app/config/participants.ts
+++ app/config/participants.ts
@@ -38,6 +38,81 @@ export const participants: Participant[] = [
     image: "/participants/shau-zhang.png",
   },
   {
+    id: "zhigang-suo",
+    industry: "学术前沿 科技无界",
+    image: "/participants/zhigang-suo.png",
+  },
+  {
+    id: "hong-chen",
+    industry: "学术前沿 科技无界",
+    image: "/participants/hong-chen.png",
+  },
+  {
+    id: "renee-zhao",
+    industry: "学术前沿 科技无界",
+    image: "/participants/renee-zhao.png",
+  },
+  {
+    id: "felix-xu",
+    industry: "学术前沿 科技无界",
+    image: "/participants/felix-xu.png",
+  },
+  {
+    id: "sharon-li.png",
+    industry: "学术前沿 科技无界",
+    image: "/participants/sharon-li.png",
+  },
+  {
+    id: "yun-zhang",
+    industry: "行业荟萃 产业实践",
+    image: "/participants/yun-zhang.png",
+  },
+  {
+    id: "bill-pu",
+    industry: "行业荟萃 产业实践",
+    image: "/participants/bill-pu.png",
+  },
+  {
+    id: "qian-lin",
+    industry: "行业荟萃 产业实践",
+    image: "/participants/qian-lin.png",
+  },
+  {
+    id: "zaichang-ye",
+    industry: "行业荟萃 产业实践",
+    image: "/participants/zaichang-ye.png",
+  },
+  {
+    id: "yuandong-tian",
+    industry: "行业荟萃 产业实践",
+    image: "/participants/yuandong-tian.png",
+  },
+  {
+    id: "weiwei-hu",
+    industry: "饮水思源 母校情深",
+    image: "/participants/weiwei-hu.png",
+  },
+  {
+    id: "minming-yu",
+    industry: "饮水思源 母校情深",
+    image: "/participants/minming-yu.png",
+  },
+  {
+    id: "qihong-lin",
+    industry: "饮水思源 母校情深",
+    image: "/participants/qihong-lin.png",
+  },
+  {
+    id: "zhiwei-shan",
+    industry: "饮水思源 母校情深",
+    image: "/participants/zhiwei-shan.png",
+  },
+  {
+    id: "zujun-yu",
+    industry: "饮水思源 母校情深",
+    image: "/participants/zujun-yu.png",
+  },
+  {
     id: "more-coming",
     industry: "更多嘉宾",
     image: "/participants/more-coming.png",
